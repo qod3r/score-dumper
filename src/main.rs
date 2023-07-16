d46195e6138adfad44afe8d804f69fa8cb7d671a
qod3r <primculov@protonmail.ch>
2023-07-16 22:17:38 +0700

    automatic collection naming based on release version

--- src/main.rs
+++ src/main.rs
@@ -13,8 +13,8 @@ fn main() {
 
     // Connect to db
     let db = utils::connect_db("mongodb://localhost:27017", "osu");
-    let coll_name = "rust_buffered";
-    let coll = db.collection::<Document>(coll_name);
+    let coll_name = format!("rust_release_{}", option_env!("CARGO_PKG_VERSION").unwrap());
+    let coll = db.collection::<Document>(coll_name.as_str());
     println!("Using collection {}", coll_name);
 
     let buf_max = 30;
@@ -30,30 +30,37 @@ fn main() {
     );
     let mut prev_state = buf.get(0).expect("Can't buf.get(0)").menu.state;
     let mut last_submitted_sum = u32::MAX;
+    // prevent logging spam when gosu freaks out at aspire maps
+    let mut skipped_recently = false;
     loop {
         let msg = read_once(&mut socket);
         let frame: Model = match serde_json::from_str(&msg) {
             Ok(f) => f,
             Err(e) => {
+                if skipped_recently {
+                    continue;
+                }
                 println!("Can't parse to Model: {}", e.to_string());
+                println!("Skipping frame...");
+                skipped_recently = true;
                 continue;
             }
         };
-        let curr_state = frame.menu.state;
+        skipped_recently = false;
 
-        // // skip if the game is closed
-        // if curr_state == -1 {
-        //     // TODO: panic for now
-        //     panic!("osu! is closed");
-        // }
+        let curr_state = frame.menu.state;
+        // skip if the game is closed
+        if curr_state == -1 {
+            continue;
+        }
 
         // keep the last `buf_max` frames
         if buf.len() >= buf_max {
             buf.pop_front();
         }
         buf.push_back(frame);
-        let curr_frame = buf.back().expect("Can't get back of buf");
 
+        let curr_frame = buf.back().expect("Can't get back of buf");
         // search for a submittable score when
         // state changed from 2 (quit or finish)
         // state didn't change but gameplay values did (restart)
@@ -70,23 +77,9 @@ fn main() {
                 // BUG: valid subsequent scores with the same score and hit_sum are not submitted
                 // probably not that important since it's very rare
                 last_submitted_sum = max.gameplay.sum();
-
-                // clear the buffer except the last two frames
-                // let mut drained: VecDeque<Model> = buf.drain(buf.len() - 2..).collect();
-                // buf.append(&mut drained);
-
-                // clear the buffer except the last (empty) frame
-                // let last = buf.pop_back().unwrap();
-                // buf.push_back(last);
-
-                // clear the buffer and don't keep frames
                 buf.clear();
-                // println!("after clear: {:#?}", buf);
             }
-            // else
-            // println!("buf does not have submittable scores, skipping...");
         }
-
         prev_state = curr_state;
     }
 }
