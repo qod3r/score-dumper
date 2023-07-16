c075052e352c62ce377a94001909e6af900183a0
qod3r <primculov@protonmail.ch>
2023-07-16 19:17:39 +0700

    fix score duplication when retrying from results screen

--- src/main.rs
+++ src/main.rs
@@ -10,21 +10,26 @@ use utils::read_once;
 fn main() {
     // Connect to gosumemory
     let mut socket = utils::connect_gosu("ws://localhost:24050/ws");
-
+    
     // Connect to db
     let db = utils::connect_db("mongodb://localhost:27017", "osu");
-    let coll = db.collection::<Document>("rust_release_0.2.0");
+    let coll_name = "rust_buffered";
+    let coll = db.collection::<Document>(coll_name);
+    println!("Using collection {}", coll_name);
+
+    let buf_max = 30;
+    println!("Using buffer with size {}", buf_max);
     println!("Ready.");
 
     // use a buffer of frames to compare against
     // because using just two frames sometimes results in some weird stuff happening
-    let buf_max = 10;
     let mut buf: VecDeque<Model> = VecDeque::with_capacity(buf_max + 1);
     // have at least two frames before logic, a bit janky but whatever
     buf.push_back(
         serde_json::from_str::<Model>(&read_once(&mut socket)).expect("Can't parse to Model"),
     );
     let mut prev_state = buf.get(0).expect("Can't buf.get(0)").menu.state;
+    let mut last_submitted_sum = u32::MAX;
     loop {
         let msg = read_once(&mut socket);
         let frame: Model = serde_json::from_str(&msg).expect("Can't parse to Model");
@@ -43,32 +48,33 @@ fn main() {
         buf.push_back(frame);
         let curr_frame = buf.back().expect("Can't get back of buf");
 
-        // wait for gameplay
-        // check if stuff is zero
-        // iterate and compare
-        // select the best candidate and submit
-
         // search for a submittable score when
         // state changed from 2 (quit or finish)
         // state didn't change but gameplay values did (restart)
         if (prev_state == 2 && curr_state != 2) || (curr_frame.gameplay.sum() == 0) {
             let max = buf
                 .iter()
-                .max_by_key(|f| &f.gameplay)
+                .max_by_key(|f| &f.gameplay) // if several frames are equal, returns the most recent one
                 .expect("Couldn't get max by key");
-            if max.gameplay.sum() != 0 {
+            if (max.gameplay.sum() != 0) && (max.gameplay.sum() != last_submitted_sum) {
                 utils::print_score(max);
-                // println!("{:#?}", buf);
-
-                // // clear the buffer except the last two frames to keep the logic going
-                // let mut drained: VecDeque<Model> = buf.drain(buf.len() - 2..).collect();
+                utils::dump_to_db(max, &coll);
                 
+                // TODO: figure out how to not duplicate a submit after retrying from the results screen instead of checking the sum
+                // BUG: valid subsequent scores with the same score and hit_sum are not submitted
+                // probably not that important since it's very rare
+                last_submitted_sum = max.gameplay.sum();
+
+                // clear the buffer except the last two frames
+                // let mut drained: VecDeque<Model> = buf.drain(buf.len() - 2..).collect();
+                // buf.append(&mut drained);
+
                 // clear the buffer except the last (empty) frame
                 // let last = buf.pop_back().unwrap();
-                buf.clear();
-                // buf.append(&mut drained);
                 // buf.push_back(last);
-                
+
+                // clear the buffer and don't keep frames
+                buf.clear();
                 // println!("after clear: {:#?}", buf);
             }
             // else
@@ -77,46 +83,4 @@ fn main() {
 
         prev_state = curr_state;
     }
-
-    // let first_frame: Model =
-    //     serde_json::from_str(&read_once(&mut socket)).expect("Can't parse to Model");
-
-    // let mut prev_state = first_frame.menu.state;
-    // let mut prev_frame = first_frame;
-    // let mut prev_hits = prev_frame.gameplay.hits.hit_sum();
-    // let mut last_dumped_hits = u32::MAX;
-    // loop {
-    //     // get ws message
-    //     let msg = read_once(&mut socket);
-
-    //     // parse to Model
-    //     let curr_frame: Model = serde_json::from_str(&msg).expect("Can't parse to Model");
-
-    //     // get new state
-    //     let curr_state = curr_frame.menu.state;
-    //     let curr_hits = curr_frame.gameplay.hits.hit_sum();
-
-    //     // a play is valid if:
-    //     // the play has at least 1 hit
-    //     // AND either
-    //     // state changed from 2 (gameplay ended)
-    //     // OR
-    //     // hitcount reset (map restarted, state remained 2)
-
-    //     // BUG: quick retry submits big combo 0 acc plays (all hits are 300's)
-    //     //      probably another gosumemory quirk
-    //     //      maybe i should count hits with something else
-    //     //      i swear gosumemory has so many edge cases 💀
-    //     if (prev_hits > 0 && (last_dumped_hits != prev_hits))
-    //         && (((curr_state != prev_state) && (prev_state == 2)) || (curr_hits == 0))
-    //     {
-    //         utils::print_score(&prev_frame);
-    //         utils::dump_to_db(&prev_frame, &coll);
-    //         last_dumped_hits = prev_hits;
-    //     }
-
-    //     prev_state = curr_state;
-    //     prev_frame = curr_frame;
-    //     prev_hits = curr_hits;
-    // }
 }
