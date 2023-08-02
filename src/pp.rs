977cd6506e3a66ef43ac0497c07123f7d649f4b5
qod3r <primculov@protonmail.ch>
2023-08-02 21:40:12 +0700

    remove overengineered bullshit

--- src/pp.rs
+++ src/pp.rs
@@ -1,52 +1,72 @@
 use crate::model;
-use rosu_pp::{
-    osu::OsuGradualPerformanceAttributes, osu::OsuScoreState, Beatmap, BeatmapExt,
-    PerformanceAttributes,
-};
+use rosu_pp::{osu::OsuScoreState, Beatmap, OsuPP};
 
-// beware: actual schizo comments below
-
-pub struct PPCalc<'a> {
+pub struct PPCalc {
     map: Beatmap,
-    result: Option<PerformanceAttributes>,
-    gradual: Option<OsuGradualPerformanceAttributes<'a>>,
+    mods: u32,
 }
 
-// [~1am] HOLY SHIT
-impl<'a> PPCalc<'a> {
-    pub fn for_path(path: &String) -> Result<Self, ()> {
-        // [~10pm]               let's avoid lifetimes
-        match Beatmap::from_path(path.clone()) {
-            // not sure if this is jank af
-            Ok(bm) => Ok(Self {
-                map: bm,
-                result: None,
-                gradual: None,
-            }),
+impl PPCalc {
+    pub fn from_path(path: &String, mods: u32) -> Result<Self, ()> {
+        match Beatmap::from_path(path) {
+            Ok(bm) => Ok(Self { map: bm, mods }),
             Err(e) => Err(println!("{}", e)),
         }
     }
 
-    // i cannot figure out how to do this in the `for_path` 💀
-    pub fn create_gradual(&'a mut self, frame: model::Model) {
-        self.gradual = Some(OsuGradualPerformanceAttributes::new(
-            &self.map,
-            frame.menu.mods.num,
-        ));
+    fn get_state(frame: &model::Model) -> OsuScoreState {
+        let g = &frame.gameplay;
+        OsuScoreState {
+            max_combo: g.combo.max as usize,
+            n300: g.hits.count_300 as usize,
+            n100: g.hits.count_100 as usize,
+            n50: g.hits.count_50 as usize,
+            n_misses: g.hits.count_miss as usize,
+        }
     }
 
-    pub fn calc(&'a mut self, frame: model::Model) -> model::gameplay::PP {
-        // TODO: calc for curr, fc and (separately) ss and return PP
-        self.gradual
-            .as_mut()
-            .unwrap()
-            .process_next_object(OsuScoreState {
-                max_combo: 0,
-                n300: 0,
-                n100: 0,
-                n50: 0,
-                n_misses: 0,
-            });
-        todo!()
+    fn remove_misses(state: &mut OsuScoreState, frame: &model::Model) {
+        // assume misses are instead 300's
+        state.n300 += state.n_misses;
+        state.n_misses = 0;
+        state.max_combo = frame.menu.bm.stats.max_combo as usize
+    }
+
+    pub fn current(&self, frame: &model::Model) -> f64 {
+        let state = Self::get_state(frame);
+        
+        OsuPP::new(&self.map)
+            .mods(self.mods)
+            .state(state)
+            .calculate()
+            .pp()
+    }
+
+    pub fn fc(&self, frame: &model::Model) -> f64 {
+        let mut state = Self::get_state(frame);
+        Self::remove_misses(&mut state, frame);
+
+        OsuPP::new(&self.map)
+            .mods(self.mods)
+            .state(state)
+            .calculate()
+            .pp()
+    }
+
+    // not very efficient i know
+    pub fn ss(&self) -> f64 {
+        OsuPP::new(&self.map)
+            .mods(self.mods)
+            .accuracy(100.0)
+            .calculate()
+            .pp()
+    }
+
+    pub fn pp(&self, frame: &model::Model) -> model::gameplay::PP {
+        model::gameplay::PP {
+            current: self.current(frame),
+            fc: self.fc(frame),
+            ss: self.ss()
+        }
     }
 }
